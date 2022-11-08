53d58e9076ea3c9a9af7f97172ac43d4adf929d5
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2022-11-08 16:09:40 -0300

    Add datetime, Modify theme styles, Add edit form actions

--- js/global.js
+++ js/global.js
@@ -1,4 +1,16 @@
 const targetBlank = document.querySelectorAll("a[target='_blank']");
+const formAdd = document.querySelector('#form-add');
+const inputAdd = document.querySelector('#input-add');
+const formEdit = document.querySelector('#form-edit');
+const inputEdit = document.querySelector('#input-edit');
+const cancelEdit = document.querySelector('#cancel-edit');
+const formSearch = document.querySelector('#form-search');
+const formFilter = document.querySelector('#form-filter');
+const taskList = document.querySelector('#task-list');
+
+// iniciadas para edição da tarefa
+let taskTitle;
+let dataTaskId;
 
 /**
  * ADICIONA ÍCONE AOS LINKS COM TARGET BLANK
@@ -7,3 +19,171 @@ for (let i = 0; i < targetBlank.length; i++) {
    targetBlank[i].innerHTML +=
       '<i class="fa-solid fa-arrow-up-right-from-square"></i>';
 }
+
+/**
+ * CRIA BOTÃO EM UM ITEM DA LISTA DE BOTÕES
+ */
+const createItemWithButton = (className, title, text) => {
+   const button = document.createElement('button');
+   button.classList.add(className);
+   button.setAttribute('title', title);
+   // texto ou tag do ícone
+   button.innerHTML = text;
+
+   const taskButtonItem = document.createElement('li');
+   taskButtonItem.appendChild(button);
+
+   return taskButtonItem;
+};
+
+/**
+ * CRIA TAREFA
+ */
+const createTask = (text) => {
+   const dateTime = [...currentDateTimeFormatted()];
+
+   // tratamento no dateTime para criar dataset como id
+   const dateString = dateTime[0].toString();
+   const timeString = dateTime[1].toString();
+   const dataDateTime =
+      dateString.replace(/\//g, '') + timeString.replace(/:/g, '');
+
+   const task = document.createElement('div');
+   task.classList.add('task');
+   task.setAttribute('data-task-id', dataDateTime);
+
+   const taskDateTime = document.createElement('span');
+   taskDateTime.classList.add('task-date-time');
+   taskDateTime.innerHTML = `${dateTime[0]} <br> ${dateTime[1]}`;
+   taskDateTime.setAttribute(
+      'title',
+      `Criada em ${dateTime[0]} às ${dateTime[1]}`
+   );
+   task.appendChild(taskDateTime);
+
+   const taskTitleEl = document.createElement('h3');
+   taskTitleEl.innerHTML = text;
+   task.appendChild(taskTitleEl);
+
+   const taskButtons = document.createElement('ul');
+   taskButtons.classList.add('task-buttons');
+
+   taskButtons.appendChild(
+      createItemWithButton(
+         'finish-task',
+         'Finalizar tarefa',
+         '<i class="fa-solid fa-check"></i>'
+      )
+   );
+   taskButtons.appendChild(
+      createItemWithButton(
+         'edit-task',
+         'Editar tarefa',
+         '<i class="fa-solid fa-pen"></i>'
+      )
+   );
+   taskButtons.appendChild(
+      createItemWithButton(
+         'delete-task',
+         'Deletar tarefa',
+         '<i class="fa-solid fa-trash"></i>'
+      )
+   );
+
+   task.appendChild(taskButtons);
+   taskList.appendChild(task);
+};
+
+/**
+ * ADICIONA UMA TAREFA
+ */
+formAdd.addEventListener('submit', (event) => {
+   event.preventDefault();
+
+   const inputAddValue = inputAdd.value;
+
+   if (inputAddValue) {
+      createTask(inputAddValue);
+   }
+
+   // limpa o campo
+   inputAdd.value = '';
+   inputAdd.focus();
+});
+
+/**
+ * ALTERNA ENTRE MOSTRAR O FORMULÁRIO DE EDIÇÃO
+ * OU TODOS OS OUTROS (E LISTA DE TAREFAS)
+ */
+const toggleForms = () => {
+   formAdd.classList.toggle('hide');
+   formEdit.classList.toggle('hide');
+   formSearch.classList.toggle('hide');
+   formFilter.classList.toggle('hide');
+   taskList.classList.toggle('hide');
+};
+
+/**
+ * AÇÕES DOS BOTÕES DA TAREFA
+ */
+document.addEventListener('click', (event) => {
+   const task = event.target.closest('.task');
+
+   // armazena o título da tarefa
+   if (task) {
+      taskTitle = task.querySelector('h3').innerText;
+   }
+
+   // finaliza ou recupera a tarefa
+   if (event.target.classList.contains('finish-task')) {
+      task.classList.toggle('done');
+   }
+   // edita a tarefa
+   if (event.target.classList.contains('edit-task')) {
+      toggleForms();
+
+      inputEdit.value = taskTitle;
+      dataTaskId = task.dataset.taskId;
+   }
+   // deleta a tarefa
+   if (event.target.classList.contains('delete-task')) {
+      task.remove();
+   }
+});
+
+/**
+ * ATUALIZA TAREFA
+ */
+const updateTask = (text) => {
+   const allTasks = document.querySelectorAll('.task');
+
+   allTasks.forEach((task) => {
+      if (task.dataset.taskId === dataTaskId) {
+         task.querySelector('h3').innerText = text;
+      }
+   });
+};
+
+/**
+ * EDITA UMA TAREFA
+ */
+formEdit.addEventListener('submit', (event) => {
+   event.preventDefault();
+
+   const inputEditValue = inputEdit.value;
+
+   if (inputEditValue) {
+      updateTask(inputEditValue);
+   }
+
+   toggleForms();
+});
+
+/**
+ * BOTÃO CANCELAR EDIÇÃO DA TAREFA
+ */
+cancelEdit.addEventListener('click', (event) => {
+   event.preventDefault();
+
+   toggleForms();
+});
--- js/themes.js
+++ js/themes.js
@@ -2,34 +2,31 @@ const formThemeLabel = document.querySelector('#form-theme label');
 const checkboxTheme = document.querySelector('#checkbox-theme');
 const themeBall = document.querySelector('.theme-ball');
 const taskContainer = document.querySelector('#task-container');
-const taskTodoH3 = document.querySelectorAll('.task-todo h3');
+const taskTodoH3 = document.querySelectorAll('.task h3');
+
+/**
+ * DEFINE PROPRIEDADE NO BODY
+ */
+const setPropertyInBody = (propertyName, value) => {
+   document.body.style.setProperty(propertyName, value);
+};
 
 /**
  * MODIFICA O TEMA
  */
 checkboxTheme.addEventListener('change', () => {
+   // checked modifica para o tema escuro
    if (checkboxTheme.checked) {
-      // checked modifica para o tema escuro
       themeBall.style.transform = 'translate(24px)';
       formThemeLabel.setAttribute('title', 'Trocar para tema claro');
-      taskContainer.style.backgroundColor = 'var(--color-background-dark)';
-      taskTodoH3.forEach((txt) => {
-         txt.style.color = 'var(--color-secondary)';
-      });
-      document.body.style.setProperty(
-         '--color-bg-toggle',
-         'var(--color-background-dark)'
-      );
+
+      setPropertyInBody('--color-bg-toggle', 'var(--color-dark)');
+      setPropertyInBody('--color-text-toggle', 'var(--color-secondary)');
    } else {
       themeBall.style.transform = 'translate(0)';
       formThemeLabel.setAttribute('title', 'Trocar para tema escuro');
-      taskContainer.style.backgroundColor = 'var(--color-background-light)';
-      taskTodoH3.forEach((txt) => {
-         txt.style.color = 'var(--color-background-dark)';
-      });
-      document.body.style.setProperty(
-         '--color-bg-toggle',
-         'var(--color-background-light)'
-      );
+
+      setPropertyInBody('--color-bg-toggle', 'var(--color-light)');
+      setPropertyInBody('--color-text-toggle', 'var(--color-dark)');
    }
 });
