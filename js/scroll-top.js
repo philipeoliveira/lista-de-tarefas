1ccf2bda690593977d031a27721ca6fffb4b1978
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2022-12-14 20:38:50 -0300

    Remove a console.log

--- js/scroll-top.js
+++ js/scroll-top.js
@@ -4,8 +4,7 @@ window.addEventListener('scroll', () => {
    scrollTopBtn.classList.toggle('btn-scroll-top', window.scrollY > 500);
 });
 
-scrollTopBtn.addEventListener('click', (event) => {
-   console.log(event);
+scrollTopBtn.addEventListener('click', () => {
    window.scrollTo({
       top: 0,
       behavior: 'smooth',
